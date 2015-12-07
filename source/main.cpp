2c7ba70f1b9710cfe1feb9b9e606661412675b5e
xywei@FUJITSU <wxy0516@gmail.com>
2015-12-07 15:50:28 +0800

    add first test

--- source/main.cpp
+++ source/main.cpp
@@ -12,11 +12,20 @@
 #include <iostream>
 #include <boost/range/irange.hpp>
 
+#include <ParamHandler.hpp>
+
 int main (int argc, char** argv)
 {
 	std::cout << "Hello, this is ToySolver!" << std::endl;
 	std::cout << "You gave me "<< argc << " arguments:" << std::endl;
 	for (auto i : boost::irange<int> (0,argc))
 		std::cout << "# " << i << "-th: " << argv[i] << std::endl;
+
+	assert (argc==2);
+	std::string param_filename = argv[1];
+	ParamHandler parameters;
+
+	parameters.read_file (param_filename);
+	parameters.display ();
 	return 0;
 }
