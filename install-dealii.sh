824d2eb0d3fb61cac86ac27c7559ee2b9aeaae79
Xiaoyu WEI <xywei@users.noreply.github.com>
2015-12-19 15:05:05 +0800

    Update install-dealii.sh
    Optimised for multiple calls.

--- install-dealii.sh
+++ install-dealii.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 wget -nc https://github.com/dealii/dealii/releases/download/v8.3.0/dealii-8.3.0.tar.gz 
-tar -xf dealii-8.3.0.tar.gz
+if cd dealii-8.3.0; then echo ; else tar -xf dealii-8.3.0.tar.gz; fi
 mkdir -p dealii-build
 cd dealii-build
 cmake --DCMAKE_INSTALL_PREFIX=../dealii-installed ../dealii-8.3.0
 make -j2
 make install
+export DONE_WITH_DEAL_II=true
